d95fd705ed14a4c4761d62b3b59eb0326603f2ab
50134830-sudo <50134830@est.ort.edu.ar>
2026-06-19 14:16:49 -0300

    a

---
+++ Assets/Script/Collect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggertEnter(Collider other){
+        if (other.tag == Sphere){
+            Debug.Log("HEY")
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
